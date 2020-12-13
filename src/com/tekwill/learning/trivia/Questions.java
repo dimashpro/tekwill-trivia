d1f26ba3ad08446fb0ca314f0d3859ef841ca46c
dimashpro <sergiu.proca@gmail.com>
2020-12-13 20:10:21 +0100

    add questions for two levels of game

---
+++ src/com/tekwill/learning/trivia/Questions.java
@@ -0,0 +1,32 @@
+package com.tekwill.learning.trivia;
+
+public class Questions {
+    public static final String LEVEL_1_QUESTION_1 = "Which of these cities is the capital of the Republic of Moldova?";
+    public static final String LEVEL_1_QUESTION_1_ANSWER_1 = "Rome";
+    public static final String LEVEL_1_QUESTION_1_ANSWER_2 = "Vienna";
+    public static final String LEVEL_1_QUESTION_1_ANSWER_3 = "Chisinau";
+    public static final String LEVEL_1_QUESTION_1_ANSWER_4 = "Monaco";
+    public static final int LEVEL_1_QUESTION_1_SCORE = 100;
+
+    public static final String LEVEL_1_QUESTION_2 = "Who composed the masterpiece \"Luceafarul\"?";
+    public static final String LEVEL_1_QUESTION_2_ANSWER_1 = "Shakespeare";
+    public static final String LEVEL_1_QUESTION_2_ANSWER_2 = "Eminescu";
+    public static final String LEVEL_1_QUESTION_2_ANSWER_3 = "Pushkin";
+    public static final String LEVEL_1_QUESTION_2_ANSWER_4 = "Alighieri";
+    public static final int LEVEL_1_QUESTION_2_SCORE = 100;
+
+    public static final String LEVEL_1_QUESTION_3 = "What is the most dangerous breed of dog?";
+    public static final String LEVEL_1_QUESTION_3_ANSWER_1 = "chi hua hua";
+    public static final String LEVEL_1_QUESTION_3_ANSWER_2 = "doberman";
+    public static final String LEVEL_1_QUESTION_3_ANSWER_3 = "pit bull";
+    public static final String LEVEL_1_QUESTION_3_ANSWER_4 = "vasea bolea";
+    public static final int LEVEL_1_QUESTION_3_SCORE = 100;
+
+    public static final String LEVEL_2_QUESTION_1 = "What is the most useful platform to study the Java programming language?";
+    public static final String LEVEL_2_QUESTION_1_ANSWER_1 = "Facebook.com";
+    public static final String LEVEL_2_QUESTION_1_ANSWER_2 = "Instagram";
+    public static final String LEVEL_2_QUESTION_1_ANSWER_3 = "Tik Tok";
+    public static final String LEVEL_2_QUESTION_1_ANSWER_4 = "docs.oracle.com/en/java/";
+    public static final int LEVEL_2_QUESTION_1_SCORE = 100;
+
+}
