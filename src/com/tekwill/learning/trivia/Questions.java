e0a666388485b89b0102d1c3141707617033dc8e
dimashpro <sergiu.proca@gmail.com>
2020-12-27 16:56:47 +0100

    add questions for 3 levels of the game

--- src/com/tekwill/learning/trivia/Questions.java
+++ src/com/tekwill/learning/trivia/Questions.java
@@ -29,4 +29,39 @@ public class Questions {
     public static final String LEVEL_2_QUESTION_1_ANSWER_4 = "docs.oracle.com/en/java/";
     public static final int LEVEL_2_QUESTION_1_SCORE = 100;
 
+    public static final String LEVEL_2_QUESTION_2 = "Which of these is NOT an non-access modifier?";
+    public static final String LEVEL_2_QUESTION_2_ANSWER_1 = "abstract";
+    public static final String LEVEL_2_QUESTION_2_ANSWER_2 = "private";
+    public static final String LEVEL_2_QUESTION_2_ANSWER_3 = "static";
+    public static final String LEVEL_2_QUESTION_2_ANSWER_4 = "final";
+    public static final int LEVEL_2_QUESTION_2_SCORE = 100;
+
+    public static final String LEVEL_2_QUESTION_3 = "Which of these is NOT a feature and component of Java?";
+    public static final String LEVEL_2_QUESTION_3_ANSWER_1 = "encapsulation";
+    public static final String LEVEL_2_QUESTION_3_ANSWER_2 = "platform independence";
+    public static final String LEVEL_2_QUESTION_3_ANSWER_3 = "drinking beer";
+    public static final String LEVEL_2_QUESTION_3_ANSWER_4 = "object orientation";
+    public static final int LEVEL_2_QUESTION_3_SCORE = 100;
+
+
+    public static final String LEVEL_3_QUESTION_1 = "Who brings the better gifts on the Christmas?";
+    public static final String LEVEL_3_QUESTION_1_ANSWER_1 = "Santa Claus";
+    public static final String LEVEL_3_QUESTION_1_ANSWER_2 = "Mos Craciun";
+    public static final String LEVEL_3_QUESTION_1_ANSWER_3 = "Ded Moroz";
+    public static final String LEVEL_3_QUESTION_1_ANSWER_4 = "Snegurocika";
+    public static final int LEVEL_3_QUESTION_1_SCORE = 100;
+
+    public static final String LEVEL_3_QUESTION_2 = "Where are stored object reference variables?";
+    public static final String LEVEL_3_QUESTION_2_ANSWER_1 = "Stack memory";
+    public static final String LEVEL_3_QUESTION_2_ANSWER_2 = "In brain";
+    public static final String LEVEL_3_QUESTION_2_ANSWER_3 = "Heap memory";
+    public static final String LEVEL_3_QUESTION_2_ANSWER_4 = "On the roof";
+    public static final int LEVEL_3_QUESTION_2_SCORE = 100;
+
+    public static final String LEVEL_3_QUESTION_3 = "Which of these data types have smallest size?";
+    public static final String LEVEL_3_QUESTION_3_ANSWER_1 = "byte";
+    public static final String LEVEL_3_QUESTION_3_ANSWER_2 = "short";
+    public static final String LEVEL_3_QUESTION_3_ANSWER_3 = "int";
+    public static final String LEVEL_3_QUESTION_3_ANSWER_4 = "long";
+    public static final int LEVEL_3_QUESTION_3_SCORE = 100;
 }
