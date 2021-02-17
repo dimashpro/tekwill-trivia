9c170d1db405675dd129d165079319030fed9183
dimashpro <sergiu.proca@gmail.com>
2021-02-17 21:47:35 +0100

    added TriviaAdmin class

--- src/com/tekwill/learning/trivia/game/engine/TriviaAdmin.java
+++ src/com/tekwill/learning/trivia/game/engine/TriviaAdmin.java
@@ -1,2 +1,24 @@
-package com.tekwill.learning.trivia.game.engine;public class TriviaAdmin {
+package com.tekwill.learning.trivia.game.engine;
+
+import java.util.Scanner;
+
+public class TriviaAdmin {
+    public void start() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("--- Do you want to be a the Ultimate Trivia Man \uD83E\uDD13? ---");
+        do {
+            displayHelpOptionsMessage();
+            pickQuestion();
+
+            System.out.print("answer > ");
+            String userAnswer = scanner.nextLine();
+
+            validateResponse(userAnswer);
+
+            if (questionLevel >= 4)
+                endGameWithCongratulations();
+
+        } while (!gameOver);
+    }
 }
